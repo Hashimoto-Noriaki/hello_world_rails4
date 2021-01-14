b08687088f953ae17aa6d3b0dc254a45c8df34cc
橋本範昭 <nori.hasimo.89@gmail.com>
2021-01-15 00:29:03 +0900

    home_controller作成

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module HelloWorldRails4
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
+
